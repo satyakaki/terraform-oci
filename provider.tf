e256d7e28a91594869007ff0d90e53f635ede648
satyakaki <satyakaki@hotmail.com>
2023-03-19 11:37:53 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaauko6735s3ntuoyvoxemarh5rnjqfjdua3ct5ztysxvl53vryr4fa"
+  user_ocid = "ocid1.user.oc1..aaaaaaaaqtax6qpgzoyhepwwht3zguvxszxrynole2qipbzaoufl3rm35uqa" 
+  private_key_path = "/home/skkaki/.oci/terraformwsl-key.pem"
+  fingerprint = "1d:ab:a8:d9:60:71:51:99:ec:96:25:d1:86:78:08:be"
+  region = "ap-hyderabad-1"
+}
